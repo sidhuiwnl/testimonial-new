4d1eb73b36f4b05e2a6236fb5bbec789b7ecd8f4
Sidharth <sidharthinfernal@gmail.com>
2024-07-26 11:37:34 +0530

    added Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { validateRequest } from "@/lib/auth";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default async function Page() {
 	const { user } = await validateRequest();
@@ -10,8 +11,8 @@ export default async function Page() {
 	return (
 		<main className="bg-gradient-to-br from-gray-900 to-black">
 			<div className="h-screen flex flex-col items-center justify-center">
-			<h1 className="text-white text-2xl md:text-6xl font-bold text-center p-6">Add your Loved Tweets and Show the Collection</h1>
-			<Button variant="secondary">Import Tweets</Button>
+			<h1 className="text-white text-2xl md:text-6xl font-bold text-center p-6">Add your Loved Tweets and Show the Collection..</h1>
+			<Link className="text-white border-2 bg-black rounded-lg p-3" href="/Testimonial">Import Tweet</Link>
 		</div>
 		</main>
 	)
