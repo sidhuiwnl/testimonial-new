f1cda2130be081aa88dc83226afe9280fe1106de
Sidharth <sidharthinfernal@gmail.com>
2024-08-04 12:14:30 +0530

    minor style changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 import { FaGithub } from "react-icons/fa";
-
-export default function LoginPage() {
+import { AuroraBackground } from "@/components/ui/aurora-background";
+export default async function LoginPage() {
+ 
   return (
-    <main className="bg-gradient-to-br from-gray-900 to-black min-h-screen flex flex-col items-center justify-center px-4">
-      <div className="max-w-md w-full space-y-8 text-center">
-        <h1 className="text-white text-3xl md:text-5xl font-bold">
+    <AuroraBackground>
+    <main className=" w-screen bg-gradient-to-br from-gray-900 to-black min-h-screen flex flex-col items-center justify-center px-4">
+      <div className="max-w-lg w-full space-y-8 text-center">
+        <h1 className="text-white text-2xl md:text-6xl font-bold">
           Tweet Collection
         </h1>
         <p className="text-gray-400 text-xl md:text-2xl mt-4">
@@ -13,13 +15,14 @@ export default function LoginPage() {
         <div className="mt-8">
           <a
             href="/login/github"
-            className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 transition duration-150 ease-in-out"
+            className=" z-10 inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 transition duration-150 ease-in-out"
           >
-            <FaGithub className="w-6 h-6 mr-2" />
+            <FaGithub className=" z-10 w-6 h-6 mr-2" />
             
           </a>
         </div>
       </div>
     </main>
+    </AuroraBackground>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
 import { redirect } from "next/navigation";
 import { validateRequest } from "@/lib/auth";
 import Link from "next/link";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 
 export default async function Page() {
-	const { user } = await validateRequest();
-	if (!user) {
-		return redirect("/login");
-	}
-	return (
-		<main className="bg-gradient-to-br from-gray-900 to-black">
-			<div className="h-screen flex flex-col items-center justify-center">
-			<h1 className="text-white text-2xl md:text-6xl font-bold text-center p-6">Add your Loved Tweets and Show the Collection..</h1>
-			<Link className="text-white border-2 bg-black rounded-lg p-3" href="/Testimonial">Import Tweet</Link>
-		</div>
-		</main>
-	)
+  const { user } = await validateRequest();
+
+  if (!user) {
+    redirect("/login");
+  }
+  return (
+    <AuroraBackground>
+      <main className=" w-screen bg-gradient-to-br from-gray-900 to-black ">
+        
+          <div className="h-screen flex flex-col items-center justify-center">
+            <h1 className="text-white text-2xl md:text-6xl font-bold text-center p-6">
+              Add your Loved Tweets and Show the Collection..
+            </h1>
+            <Link
+              className=" z-10 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              href="/Testimonial"
+            >
+              Import Tweet
+            </Link>
+          </div>
+        
+      </main>
+    </AuroraBackground>
+  );
 }
